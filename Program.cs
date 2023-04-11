1fb4200ee375c340fe2a095a2aec56f8d8c7d332
HitBlast <hitblastlive@gmail.com>
2023-04-11 13:03:48 +0600

    updated license

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
 MIT License
 Copyright (c) 2023 Monzu77
+Copyright (c) 2023 HitBlast
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
